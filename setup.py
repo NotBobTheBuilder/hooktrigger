38810db0efddea7370c3a3f788688596ee0a86a0
Jack Wearden <jack@jackwearden.co.uk>
2015-04-07 19:15:47 +0100

    v0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 setup(
     name='hooktrigger',
 
-    version='0.1.0',
+    version='0.1.1',
 
     description='Run shell commands when GitHub webhooks are fired',
     long_description=long_description,
